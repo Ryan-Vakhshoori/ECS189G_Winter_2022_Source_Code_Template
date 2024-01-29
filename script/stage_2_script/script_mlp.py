5f9c68558682019416516c90ab6288e0ec3642ac
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2024-01-29 08:26:10 -0800

    stage 2 data

--- script/stage_2_script/script_mlp.py
+++ script/stage_2_script/script_mlp.py
@@ -1,4 +1,6 @@
 from code.stage_2_code.Dataset_Loader import Dataset_Loader
+from code.stage_2_code.Method_MLP import Method_MLP
+from code.stage_2_code.Result_Saver import Result_Saver
 import torch
 import numpy as np
 
@@ -12,5 +14,14 @@ if 1:
     # ---- objection initialization setction ---------------
     train_data_obj = Dataset_Loader('numbers', '')
     train_data_obj.dataset_source_folder_path = '../../data/stage_2_data/'
-    train_data_obj.dataset_source_file_name = 'test.csv'
-    x = train_data_obj.load()
+    train_data_obj.dataset_source_file_name = 'train.csv'
+    # x = train_data_obj.load()
+
+    method_obj = Method_MLP('multi-layer perceptron', '')
+
+    result_obj = Result_Saver('saver', '')
+    result_obj.result_destination_folder_path = '../../result/stage_2_result/MLP_'
+    result_obj.result_destination_file_name = 'prediction_result'
+
+    evaluate_obj = Evaluate_Accuracy('accuracy', '')
+    # ------------------------------------------------------
