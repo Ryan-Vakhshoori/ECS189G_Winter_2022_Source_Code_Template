a9fed46bdff81018fcc6950a9a63b1dfb0d67286
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2024-02-24 17:32:35 -0800

    stemmed

--- code/stage_4_code/Dataset_Loader.py
+++ code/stage_4_code/Dataset_Loader.py
@@ -1,7 +1,9 @@
 from code.base_class.dataset import dataset
 import os
-import nltk
 from nltk.tokenize import word_tokenize
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 class Dataset_Loader(dataset):
     data = None
@@ -21,8 +23,19 @@ class Dataset_Loader(dataset):
             file.close()
             # split into words by white space
             tokens = word_tokenize(text)
-            words = [word for word in tokens if word.isalpha()]
-            print(words[:100])
+            # convert to lower case
+            tokens = [w.lower() for w in tokens]
+            # remove punctuation from each word
+            table = str.maketrans('', '', string.punctuation)
+            stripped = [w.translate(table) for w in tokens]
+            # remove remaining tokens that are not alphabetic
+            words = [word for word in stripped if word.isalpha()]
+            # filter out stop words
+            stop_words = set(stopwords.words('english'))
+            words = [w for w in words if not w in stop_words]
+            porter = PorterStemmer()
+            stemmed = [porter.stem(word) for word in words]
+            print(stemmed[:100])
             # checking if it is a file
             if os.path.isfile(f):
                 print(f)
