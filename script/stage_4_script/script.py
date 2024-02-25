303f5c5fe8a928e34c8a1fad2c90f5f2dc4380e6
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2024-02-24 16:55:55 -0800

    setting up

--- script/stage_4_script/script.py
+++ script/stage_4_script/script.py
@@ -1,5 +1,8 @@
 from code.stage_4_code.Dataset_Loader import Dataset_Loader
+from code.stage_4_code.Method_RNN import Method_RNN
+from code.stage_4_code.Result_Saver import Result_Saver
 from code.stage_4_code.ModelExecution import ModelExecution
+from code.stage_4_code.Evaluation_Metrics import Evaluation_Metrics
 import numpy as np
 
 if 1:
@@ -14,6 +17,13 @@ if 1:
     test_data_obj = Dataset_Loader('numbers', '')
     test_data_obj.dataset_source_folder_path = '../../data/stage_4_data/text_classification/test'
 
-    setting_obj = ModelExecution('model execution', '')
+    method_obj = Method_RNN()
 
-    setting_obj.prepare(loaded_obj, method_obj, result_obj, evaluate_obj)
+    result_obj = Result_Saver('saver', '')
+    result_obj.result_destination_folder_path = '../../result/stage_4_result/RNN_'
+    result_obj.result_destination_file_name = 'prediction_result'
+    setting_obj = ModelExecution('model execution', '')
+    evaluate_obj = Evaluation_Metrics('accuracy, precision, f1 score, recall', '')
+
+    result_obj.result_destination_file_name = 'prediction_result_1'
+    setting_obj.prepare(train_data_obj, method_obj, result_obj, evaluate_obj)
