3d101a1da1c723a99aab489a68bfdd0506dc1946
Yash <yinani@ucdavis.edu>
2024-03-02 20:15:19 -0800

    with GRU

--- code/stage_4_code/Method_RNN_TG.py
+++ code/stage_4_code/Method_RNN_TG.py
@@ -20,7 +20,7 @@ class Method_RNN_TG(method, nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.optimizer = optimizer
-        self.rnn = nn.LSTM(input_size=6478, hidden_size=self.hidden_size, num_layers=2, batch_first=True) # try 100 with more epochs
+        self.rnn = nn.GRU(input_size=6478, hidden_size=self.hidden_size, num_layers=2, batch_first=True) # try 100 with more epochs
         self.fc = nn.Linear(self.hidden_size, 6478)
         self.word_to_one_hot = word_to_one_hot
         self.one_hot_to_word = one_hot_to_word
