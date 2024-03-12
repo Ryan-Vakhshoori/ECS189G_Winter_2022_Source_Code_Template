dc502122e4a81f8fa78722ea76992d4e66622e1a
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2024-03-11 21:18:14 -0700

    changed all models to 2 layers

--- code/stage_5_code/Method_GNN.py
+++ code/stage_5_code/Method_GNN.py
@@ -15,7 +15,7 @@ from code.stage_5_code.Layers import GraphConvolution
 
 class Method_GNN(method, nn.Module):
     data = None
-    max_epoch = 20
+    max_epoch = 35
     learning_rate = 1e-3
 
     def __init__(self, mName, mDescription, hidden_size, num_layers, optimizer, activation_function):
